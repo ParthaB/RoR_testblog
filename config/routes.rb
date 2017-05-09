b98f2ac506c38801a517932ffbb376b65c8f3285
ParthaB <pbp2005@gmail.com>
2017-05-09 19:58:56 -0400

    modified5-9-17

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # root of the site routed with 'root'
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
   # example of a regular route
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
+  #example resource
+  resources :posts
 end
